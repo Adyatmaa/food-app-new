c3e2f4eeb1e9a927885af9581221af7dee7cc6ce
zeaalghi <zeaalghifari1@gmail.com>
2024-06-19 20:23:24 +0700

    login register

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodapp_new/spref.dart';
-import 'package:foodapp_new/view/splash.dart';
+import 'package:foodapp_new/tree.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPref.init();
+  await Firebase.initializeApp();
   bool tema = await isTheme();
   runApp(MyWidget(tema: tema));
 }
@@ -22,7 +25,7 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
+      home: tree(),
       debugShowCheckedModeBanner: false,
       theme: SharedPref.pref?.getBool('tema') ?? false
           ? ThemeData.light()
---
+++ lib/tree.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:foodapp_new/view/home.dart';
+import 'package:foodapp_new/view/login.dart';
+import 'package:foodapp_new/view/splash.dart';
+import 'package:foodapp_new/view_model/fetch_login.dart';
+
+
+Future<void> main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp( tree());
+}
+class tree extends StatefulWidget {
+  tree({super.key});
+
+  @override
+  State<tree> createState() => _treeState();
+}
+
+class _treeState extends State<tree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: Auth().authStateChanges,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child:SplashScreen());
+            }
+      if(snapshot.hasData){
+      return HomeScreen();
+      }else{
+       return Login();
+      }
+    },);
+  }
+}
